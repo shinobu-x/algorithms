a6a383b4d780e9ffd2e83a5decee47ea64394cb1
Shinobu KINJO <kinjo@sensetime.com>
2020-11-14 13:45:12 +0900

    modified:   union_find/union_find.hpp 	new file:   union_find/union_find_by_size.hpp

--- union_find/union_find.hpp
+++ union_find/union_find.hpp
@@ -4,24 +4,16 @@
 #ifndef UNION_FIND_HPP
 #define UNION_FIND_HPP
 struct UnionFind {
-  std::vector<int> vertex;
-  UnionFind(std::size_t vertices) : vertex(vertices, -1) {}
-  auto root(int index) -> int {
-    return (vertex[index] < 0 ? index : vertex[index] = root(vertex[index]));
-  }
+  std::vector<int> data;
+  UnionFind(int size) : data(size, -1) {}
+  auto find(int x) -> int {return (data[x] < 0) ? x : data[x] = find(data[x]);}
   auto unite(int x, int y) -> bool {
     x = find(x), y = find(y);
     if (x != y) {
-      if (vertex[x] > vertex[y]) std::swap(x, y);
-      vertex[x] += vertex[y]; vertex[y] = x;
+      if (data[x] > data[y]) std::swap(x, y);
+      data[x] += data[y]; data[y] = x;
       return false;
     } else return true;
-  }
-  auto find(int x) -> int {
-    return (vertex[x] < 0) ? x : vertex[x] = find(vertex[x]);
-  }
-  auto size(int x) -> int {
-    return -vertex[find(x)];
   }
   auto same(int x, int y) -> bool {
     return find(x) == find(y);
---
+++ union_find/union_find_by_size.hpp
@@ -0,0 +1,20 @@
+#include <utility>
+#include <vector>
+
+#ifndef UNION_FIND_BY_SIZE_HPP
+#define UNION_FIND_BY_SIZE_HPP
+struct UnionFindBySize {
+  std::vector<int> data;
+  std::vector<int> size;
+  UnionFindBySize(int size) : data(size, -1), size(size, 0) {}
+  auto find(int x) -> int {return (data[x] < 0) ? x : data[x] = find(data[x]);}
+  auto unite(int x, int y) -> bool {
+    if (x != y) {
+      if (size[x] < size[y]) { size[y] += size[x]; data[x] = y; }
+      else { size[x] += size[y]; data[y] = x; }
+      return true;
+    } return false;
+  }
+  auto same(int x, int y) -> bool {return find(x) == find(y);}
+};
+#endif
