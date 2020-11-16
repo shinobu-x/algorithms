89420c2e476e88198d64359fc22abc2b2290dbca
Shinobu KINJO <kinjo@sensetime.com>
2020-11-16 09:53:41 +0900

    modified:   bfs.hpp

--- bfs.hpp
+++ bfs.hpp
@@ -9,14 +9,17 @@ template <typename T>
 struct BFS {
   Graph<T> graph;
   std::vector<T> dist;
-  std::vector<std::queue<int>> queue;
   const T inf = std::numeric_limits<T>::max();
   T cost_max = 0, upper_bound = 0; int size;
-  BFS(const Graph<T>& graph, int size) : graph(graph), dist(graph.size(), inf),
-    queue(cost_max + 1), size(size) {}
+  BFS(const Graph<T>& graph) : graph(graph), dist(graph.size(), inf),
+    size(graph.size()) {
+      for (auto& edges : graph.graph)
+        for(auto& edge : edges) cost_max = std::max(cost_max, edge.cost);
+      ++cost_max; dist[size] = 0;
+    }
   auto operator()() -> std::vector<T> {
-    dist[size] = 0;
-    queue[0].empalce(size);
+    std::vector<std::queue<int>> queue(cost_max + 1);
+    queue[0].emplace(size);
     for (T cost = 0; cost <= upper_bound; ++cost) {
       auto& q = queue[cost % cost_max];
       while (!queue.empty()) {
