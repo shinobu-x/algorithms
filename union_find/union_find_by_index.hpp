c3d09cb51638dce73960326aec9358a120b1dbc8
Shinobu KINJO <kinjo@sensetime.com>
2020-11-20 17:10:02 +0900

    modified:   union_find/union_find.hpp 	new file:   union_find/union_find_by_index.hpp

--- union_find/union_find_by_index.hpp
+++ union_find/union_find_by_index.hpp
@@ -1,17 +1,16 @@
 #include <utility>
 #include <vector>
-#ifndef UNION_FIND_HPP
-#define UNION_FIND_HPP
-struct UnionFind {
-  std::vector<int> data;
-  UnionFind(int size) : data(size, -1) {}
+#ifndef UNION_FIND_BY_INDEX_HPP
+#define UNION_FIND_BY_INDEX_HPP
+struct UnionFindByIndex {
+  std::vector<int> data, index;
+  UnionFindByIndex(int size) : data(size, -1), index(size, -1) {}
   auto find(int x) -> int {return (data[x] < 0) ? x : data[x] = find(data[x]);}
   auto unite(int x, int y) -> bool {
     x = find(x), y = find(y);
     if (x != y) {
-      if (data[x] > data[y]) std::swap(x, y);
-      data[x] += data[y]; data[y] = x;
-      return true;
+      if (index[x] > index[y]) std::swap(x, y);
+      data[x] = y; return true;
     } return false;
   }
   auto same(int x, int y) -> bool {return find(x) == find(y);}
